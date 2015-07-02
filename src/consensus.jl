f27b6e0a1bdb33f68b04bfea0983236f541c7234
Joey Krug <joeykrug@gmail.com>
2015-07-02 00:47:52 -0700

    mods to clustering

--- src/consensus.jl
+++ src/consensus.jl
@@ -50,9 +50,9 @@ function process(clusters::Vector{ClusterNode},
         best = mode
         bestClusters = clusters
     end
-    if L2dist(mode.meanVec, outcomes) > 1.07 && times == 1
-        cluster(features, rep; times=2, threshold=threshold*3)
-    else
+    #if L2dist(mode.meanVec, outcomes) > 1.07 && times == 1
+    #    cluster(features, rep; times=2, threshold=threshold*3)
+    if true
         for i = 1:length(bestClusters)
             bestClusters[i].dist = L2dist(best.meanVec, bestClusters[i].meanVec)
         end
@@ -64,7 +64,7 @@ function process(clusters::Vector{ClusterNode},
         end
         repVector = zeros(numReporters)
         for i = 1:length(distMatrix)
-            repVector[i] = 1 - distMatrix[i] / (maximum(distMatrix) + 0.00000001)
+            repVector[i] = 1 - distMatrix[i] / maximum(distMatrix)
         end
         normalize(repVector)
     end
@@ -75,12 +75,12 @@ function cluster(features::Matrix{Float64},
                  times::Int=1,
                  threshold::Float64=0.50)
     # cluster the rows of the "features" matrix
-    if threshold == 0.50
-        threshold = log10(size(features, 2)) / 1.77
-        if threshold == 0
-            threshold = 0.3
-        end
-    end
+    #if threshold == 0.50
+    #    threshold = log10(size(features, 2)) / 1.77
+    #    if threshold == 0
+    #        threshold = 0.3
+    #    end
+    #end
     clusters = ClusterNode[]
     for i = 1:length(rep)
         if rep[i] == 0.0
@@ -121,12 +121,12 @@ end
 function clusterfeck(features::Matrix{Float64},
                      rep::Vector{Float64};
                      threshold::Float64=0.50)
-    if threshold == 0.50
-        threshold = log10(size(features, 2)) / 1.77
-        if threshold == 0
-            threshold = 0.3
-        end
-    end
+    #if threshold == 0.50
+    #    threshold = log10(size(features, 2)) / 1.77
+    #    if threshold == 0
+    #        threshold = 0.3
+    #    end
+    #end
     global best, bestDist, bestClusters
     best = nothing
     bestDist = Inf
